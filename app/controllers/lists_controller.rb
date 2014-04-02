cd45ec79f4bd807242e69a5ff3f63267bb5d15d9
Mark Miyashita <negativetwelve@gmail.com>
2014-04-02 00:19:20 -0700

    Add show method with blank view for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,13 @@
 class ListsController < ApplicationController
+  before_filter :list, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def list
+    @list = List.find(params[:id])
+  end
+
 end
