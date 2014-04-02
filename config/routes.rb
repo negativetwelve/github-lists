478cca9107798853aedcaa73debd5f200b411ec2
Mark Miyashita <negativetwelve@gmail.com>
2014-04-01 23:05:05 -0700

    Add routes for the oauth

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ GitLists::Application.routes.draw do
 
   devise_for :users, path: '',
                      path_names: { sign_in: 'login', sign_up: 'sign_up', sign_out: 'logout'}
+
+  match 'auth/:provider/callback', to: 'sessions#create_github'
+  match 'auth/failure', to: redirect('/')
 end
