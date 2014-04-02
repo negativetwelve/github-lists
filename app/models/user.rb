5b23c2b928fc36ea2f719f8fee9a0cb2c4af66d7
Mark Miyashita <negativetwelve@gmail.com>
2014-04-01 23:49:10 -0700

    Collect all the free github information!

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable
+         :omniauthable, omniauth_providers: [:github]
 
   def self.find_for_github_oauth(auth)
+    puts auth
+    puts auth.extra
     where(auth.slice(:provider, :uid)).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
+        user.name = auth.info.name
+        user.nickname = auth.info.nickname
+        user.bio = auth.extra.raw_info.bio
+        user.blog = auth.extra.raw_info.blog
+        user.location = auth.extra.raw_info.location
+        user.image_url = auth.info.image
     end
   end
 
