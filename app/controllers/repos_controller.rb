a5e633b98b43b4ad43d700c6595c908b5ec67bc6
Mark Miyashita <negativetwelve@gmail.com>
2014-04-02 00:31:52 -0700

    Add repos index view

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,13 +1,21 @@
 class ReposController < ApplicationController
   before_filter :repo, only: [:show]
+  before_filter :repos, only: [:index]
 
   def show
+  end
+
+  def index
   end
 
   private
 
   def repo
     @repo = Repo.find(params[:id])
+  end
+
+  def repos
+    @repos = Repo.all
   end
 
 end
