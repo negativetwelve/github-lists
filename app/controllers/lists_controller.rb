1583a08fb945ae2932776a0b4489c5081304ca96
Mark Miyashita <negativetwelve@gmail.com>
2014-04-02 00:29:47 -0700

    Add top lists view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,21 @@
 class ListsController < ApplicationController
   before_filter :list, only: [:show]
+  before_filter :lists, only: [:index]
 
   def show
+  end
+
+  def index
   end
 
   private
 
   def list
     @list = List.find(params[:id])
+  end
+
+  def lists
+    @lists = List.all
   end
 
 end
