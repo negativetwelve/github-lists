9e87037f79da9ef71289ce6f0626ac0a91b9542b
Mark Miyashita <negativetwelve@gmail.com>
2014-04-01 22:15:06 -0700

    Change name to GitLists

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-RailsStarterApp::Application.configure do
+GitLists::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-RailsStarterApp::Application.configure do
+GitLists::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
