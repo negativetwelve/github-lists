d5c7a4fd28bf94edbec2c45f679b543cb0933112
Mark Miyashita <negativetwelve@gmail.com>
2014-04-01 23:23:39 -0700

    More github oauth authentication methods

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ GitLists::Application.routes.draw do
 
   devise_for :users, path: '',
                      path_names: { sign_in: 'login', sign_up: 'sign_up', sign_out: 'logout'},
-                     controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+                     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 
 end
