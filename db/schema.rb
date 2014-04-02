a7be5bcb4bbb359cfef806f1f17ff6b14a139fd2
Mark Miyashita <negativetwelve@gmail.com>
2014-04-01 23:56:28 -0700

    Add back removed migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402063859) do
+ActiveRecord::Schema.define(version: 20140402065348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,14 +29,14 @@ ActiveRecord::Schema.define(version: 20140402063859) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "provider"
-    t.string   "uid"
     t.string   "name"
     t.string   "nickname"
     t.string   "image_url"
     t.text     "bio"
     t.string   "blog"
     t.string   "location"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
